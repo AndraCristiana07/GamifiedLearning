e71e32c550fb6eda5ed0de683cbfbb466ca04cd8
AndraCristiana07 <taporandra@gmail.com>
2025-11-12 21:40:52 +0000

    register page + login working

--- gamified-learning-frontend/app/login/page.tsx
+++ gamified-learning-frontend/app/login/page.tsx
@@ -6,46 +6,69 @@ export default function LoginPage(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
-    // TODO: hanfdle submit 
-     
+    const [loading, setLoading] = useState(false);
+    const handleSubmit = async (e: React.FormEvent) =>{
+        e.preventDefault();
+        setError(null);
+        setLoading(true);
+
+        try {
+            const res = await fetch("http://localhost:5180/api/auth/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({email, password})
+            });
+            if (!res.ok){
+                const text = await res.text();
+                throw new Error(text || "Login failed");
+            }
+
+            const data = await res.json();
+            console.log("Login successful ", data);
+
+        } catch (err) {
+            console.error("Login error ", err);
+            setError(err.message || "Login error");
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
     <div>
       <h1>Login</h1>
       {error && <p>{error}</p>}
-      {/* on submit for form */}
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
 
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <div class="">Log in here</div>
-            <form action="#" method="POST" class="space-y-6">
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="">Log in here</div>
+            <form action="#" method="POST" className="space-y-6" onSubmit={handleSubmit}>
             <div>
-                <label for="email" class="block text-sm/6 font-medium text-gray-100">Email address</label>
-                <div class="mt-2">
-                <input id="email" type="email" name="email" required autocomplete="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email@email.com" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Email address</label>
+                <div className="mt-2">
+                <input id="email" type="email" name="email" required autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email@email.com" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                 </div>
             </div>
 
             <div>
-                <div class="flex items-center justify-between">
-                <label for="password" class="block text-sm/6 font-medium text-gray-100">Password</label>
-                <div class="text-sm">
-                    <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
+                <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Password</label>
+                <div className="text-sm">
+                    <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
                 </div>
                 </div>
-                <div class="mt-2">
-                <input id="password" type="password" name="password" required autocomplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)}  class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                <div className="mt-2">
+                <input id="password" type="password" name="password" required autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)}  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                 </div>
             </div>
 
             <div>
-                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
+                <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
             </div>
             </form>
 
-            <p class="mt-10 text-center text-sm/6 text-gray-400">
+            <p className="mt-10 text-center text-sm/6 text-gray-400">
             Don't have an account? 
-            <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300"> Register now!</a>
+            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300"> Register now!</a>
             </p>
         </div>
         </div>
---
+++ gamified-learning-frontend/app/register/page.tsx
@@ -0,0 +1,92 @@
+"use client"
+import { useState } from "react";
+
+export default function LoginPage(){
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState('')
+    
+    const handleSubmit = async (e: React.FormEvent) =>{
+        e.preventDefault();
+        setError(null);
+        setSuccess('');
+
+        try {
+            const res = await fetch("http://localhost:5180/api/auth/register", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({username, email, password})
+            });
+            if (!res.ok){
+                const text = await res.text();
+                throw new Error(text || "Register failed");
+            }
+
+            const data = await res.json();
+            console.log("Registered successful ", data);
+            setSuccess("Registered successful")
+            setUsername('')
+            setEmail('')
+            setPassword('')
+
+        } catch (err) {
+            console.error("Register error ", err);
+            setError(err.message || "Register error");
+        } 
+    }
+    return (
+    <div>
+      <h1>Login</h1>
+      {error && <p>{error}</p>}
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="">Sign up here</div>
+            {error && <p className="text-red-400 mb-4">{error} </p>}
+            {success && <p className="text-green-400 mb-4">{success}</p>}
+            <form onSubmit={handleSubmit} action="#" method="POST" className="space-y-6">
+                <div>
+                    <label htmlFor="username" className="block text-sm/6 font-medium text-gray-100">Username</label>
+                    <div className="mt-2">
+                    <input id="username" type="username" name="username" required autoComplete="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                </div>
+            </div>
+            <div>
+                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Email address</label>
+                <div className="mt-2">
+                <input id="email" type="email" name="email" required autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email@email.com" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                </div>
+            </div>
+
+            <div>
+                <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Password</label>
+                    
+                </div>
+                <div className="mt-2">
+                    <input id="password" type="password" name="password" required autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)}  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                </div>
+            </div>
+             <div>
+                <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Password</label>
+                    
+                </div>
+                <div className="mt-2">
+                    <input id="password" type="password" name="password" required autoComplete="current-password" value={password} onChange={(e) => setPassword(e.target.value)}  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                </div>
+            </div>
+
+            <div>
+                <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Register</button>
+            </div>
+            </form>
+
+            
+        </div>
+        </div>
+    </div>
+  );
+};
