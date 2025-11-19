3b9748a68db77edffc36b82614aea35a3799ec70
AndraCristiana07 <taporandra@gmail.com>
2025-11-19 19:53:28 +0000

    made code editor for code type challenges

---
+++ backend/Migrations/20251117175114_AddChallengeType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gamified_learning.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChallengeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Challenges",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Challenges");
+        }
+    }
+}
