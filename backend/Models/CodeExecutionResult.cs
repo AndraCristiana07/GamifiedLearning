8e64ae34ad8e30f19d6339ddc2a5d2443ef788a0
AndraCristiana07 <taporandra@gmail.com>
2025-11-23 19:56:13 +0000

    changed docker compose to make neccessary file for judge0 to work with code submission + enabled 2 parameteres in judge0 config file + added compile_output, token in CodeExecutionResult model

--- backend/Models/CodeExecutionResult.cs
+++ backend/Models/CodeExecutionResult.cs
@@ -2,9 +2,11 @@ namespace Gamified_learning.Models
 {
     public class CodeExecutionResult
     {
-        public string Stdout { get; set; }
-        public string Stderr { get; set; }
-        public string Output { get; set; }
+        public string? Stdout { get; set; }
+        public string? Stderr { get; set; }
+        public string? Output { get; set; }
 
+        public string? Compile_output { get; set; } 
+        public string? Token { get; set; }
     }
-};
+}
